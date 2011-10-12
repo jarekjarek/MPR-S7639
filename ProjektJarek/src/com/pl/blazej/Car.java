d3ea71c5295e546edacb4a4a03475fa2bd99b714
Jarek <jarek.gryczko@gmail.com>
2011-10-12 12:29:22 +0200

    1

---
+++ ProjektJarek/src/com/pl/blazej/Car.java
@@ -0,0 +1,20 @@
+ 
+ 
+package com.pl.blazej;
+ 
+public class Car {
+public String marka;
+public String nr;
+public Car(String marka, String nr){
+this.marka=marka;
+this.nr=nr;
+}
+public void printCar()
+{
+System.out.println("Marka: "+marka+"\t nr: "+nr);
+}
+ 
+}
+ 
+ 
+ 
---
+++ ProjektJarek/src/com/pl/blazej/Osoba.java
@@ -0,0 +1,33 @@
+
+     
+     
+    package com.pl.blazej;
+     
+    import java.util.ArrayList;
+    import java.util.List;
+     
+    public class Osoba {
+    public String imie;
+    public String nazwisko;
+    public Osoba(String imie, String nazwisko)
+    {
+    this.imie=imie;
+    this.nazwisko=nazwisko;
+    }
+    public Osoba(String imie, String nazwisko, List<Car> cars)
+    {
+    this.imie=nazwisko;
+    this.nazwisko=nazwisko;
+    this.cars=cars;
+    }
+    public List<Car> cars= new ArrayList<Car>();
+    public void printCars(){
+    for(Car c : this.cars)
+    {
+    c.printCar();
+    }
+    }
+    }
+
+
+
